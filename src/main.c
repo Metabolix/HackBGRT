0259ac5f240e5923162460d6f4b4fba442e25d90
FREEWING-JP <freewing.jp@gmail.com>
2020-11-24 11:21:55 +0900

    add JPEG function

--- src/main.c
+++ src/main.c
@@ -545,6 +545,419 @@ static BMP* LoadPNG(EFI_FILE_HANDLE root_dir, const CHAR16* path) {
 	return bmp;
 }
 
+//------------------------------------------------------------------------------
+// jpg2tga.c
+// JPEG to TGA file conversion example program.
+// Public domain, Rich Geldreich <richgel99@gmail.com>
+// Last updated Nov. 26, 2010
+//------------------------------------------------------------------------------
+#include "../picojpeg/picojpeg.h"
+
+#define STRING(var) #var
+
+#define PJPG_ENUM(DO) \
+    DO(PJPEG_OK) \
+    DO(PJPG_NO_MORE_BLOCKS) \
+    DO(PJPG_BAD_DHT_COUNTS) \
+    DO(PJPG_BAD_DHT_INDEX) \
+    DO(PJPG_BAD_DHT_MARKER) \
+    DO(PJPG_BAD_DQT_MARKER) \
+    DO(PJPG_BAD_DQT_TABLE) \
+    DO(PJPG_BAD_PRECISION) \
+    DO(PJPG_BAD_HEIGHT) \
+    DO(PJPG_BAD_WIDTH) \
+    DO(PJPG_TOO_MANY_COMPONENTS) \
+    DO(PJPG_BAD_SOF_LENGTH) \
+    DO(PJPG_BAD_VARIABLE_MARKER) \
+    DO(PJPG_BAD_DRI_LENGTH) \
+    DO(PJPG_BAD_SOS_LENGTH) \
+    DO(PJPG_BAD_SOS_COMP_ID) \
+    DO(PJPG_W_EXTRA_BYTES_BEFORE_MARKER) \
+    DO(PJPG_NO_ARITHMITIC_SUPPORT) \
+    DO(PJPG_UNEXPECTED_MARKER) \
+    DO(PJPG_NOT_JPEG) \
+    DO(PJPG_UNSUPPORTED_MARKER) \
+    DO(PJPG_BAD_DQT_LENGTH) \
+    DO(PJPG_TOO_MANY_BLOCKS22) \
+    DO(PJPG_UNDEFINED_QUANT_TABLE) \
+    DO(PJPG_UNDEFINED_HUFF_TABLE) \
+    DO(PJPG_NOT_SINGLE_SCAN) \
+    DO(PJPG_UNSUPPORTED_COLORSPACE) \
+    DO(PJPG_UNSUPPORTED_SAMP_FACTORS) \
+    DO(PJPG_DECODE_ERROR) \
+    DO(PJPG_BAD_RESTART_MARKER) \
+    DO(PJPG_ASSERTION_ERROR) \
+    DO(PJPG_BAD_SOS_SPECTRAL) \
+    DO(PJPG_BAD_SOS_SUCCESSIVE) \
+    DO(PJPG_STREAM_READ_ERROR) \
+    DO(PJPG_NOTENOUGHMEM) \
+    DO(PJPG_UNSUPPORTED_COMP_IDENT) \
+    DO(PJPG_UNSUPPORTED_QUANT_TABLE) \
+    DO(PJPG_UNSUPPORTED_MODE)
+
+#define MAKE_STRINGS(VAR) #VAR,
+const char* const PJPG_ERROR_MESSAGE[] = {
+    PJPG_ENUM(MAKE_STRINGS)
+};
+
+static CHAR16* stringtoC16(const char * const str) {
+    static CHAR16 s_tmp[32+1];
+    CHAR16* d = s_tmp;
+    char* p = (char*)str;
+    while (*p) {
+        // No Guard on Length Overflow
+        *d++ = (CHAR16)*p++;
+    }
+    *d = 0;
+    return s_tmp;
+}
+
+//------------------------------------------------------------------------------
+#ifndef max
+#define max(a,b)    (((a) > (b)) ? (a) : (b))
+#endif
+#ifndef min
+#define min(a,b)    (((a) < (b)) ? (a) : (b))
+#endif
+//------------------------------------------------------------------------------
+typedef unsigned char uint8;
+typedef unsigned int uint;
+//------------------------------------------------------------------------------
+static unsigned char *g_pInFile;
+static uint g_nInFileSize;
+static uint g_nInFileOfs;
+//------------------------------------------------------------------------------
+unsigned char pjpeg_need_bytes_callback(unsigned char* pBuf, unsigned char buf_size, unsigned char *pBytes_actually_read, void *pCallback_data)
+{
+   uint n;
+   pCallback_data;
+
+   n = min(g_nInFileSize - g_nInFileOfs, buf_size);
+
+   if ((g_nInFileOfs < 2048) || ((g_nInFileSize - g_nInFileOfs) < 2048)) {
+      Debug(L"pjpeg_need_bytes_callback: buf_size %d, n %d, %d, %d\n", buf_size, n, g_nInFileOfs, g_nInFileSize);
+   } else {
+      Debug(L".");
+   }
+
+   memcpy(pBuf, &g_pInFile[g_nInFileOfs], n);
+   *pBytes_actually_read = (unsigned char)(n);
+   g_nInFileOfs += n;
+   return 0;
+}
+//------------------------------------------------------------------------------
+// Loads JPEG image from specified file. Returns NULL on failure.
+// On success, the malloc()'d image's width/height is written to *x and *y, and
+// the number of components (1 or 3) is written to *comps.
+// pScan_type can be NULL, if not it'll be set to the image's pjpeg_scan_type_t.
+// Not thread safe.
+// If reduce is non-zero, the image will be more quickly decoded at approximately
+// 1/8 resolution (the actual returned resolution will depend on the JPEG
+// subsampling factor).
+uint8 *pjpeg_load_from_file(void* buffer, UINTN size, int *ix, int *iy, int *comps, pjpeg_scan_type_t *pScan_type, int reduce)
+{
+   pjpeg_image_info_t image_info;
+   int mcu_x = 0;
+   int mcu_y = 0;
+   uint row_pitch;
+   uint8 *pImage;
+   uint8 status;
+   uint decoded_width, decoded_height;
+   uint row_blocks_per_mcu, col_blocks_per_mcu;
+
+   *ix = 0;
+   *iy = 0;
+   *comps = 0;
+   if (pScan_type) *pScan_type = PJPG_GRAYSCALE;
+
+   g_pInFile = (void*)buffer;
+   g_nInFileOfs = 0;
+   g_nInFileSize = size;
+
+   Debug(L"pjpeg_load_from_file: Size %d.\n", size);
+   status = pjpeg_decode_init(&image_info, pjpeg_need_bytes_callback, NULL, (unsigned char)reduce);
+   if (status)
+   {
+      Print(L"pjpeg_decode_init() failed with status %u(%s)\n", status, stringtoC16(PJPG_ERROR_MESSAGE[status]));
+
+      if (status == PJPG_UNSUPPORTED_MODE)
+      {
+         Print(L"Progressive JPEG files are not supported.\n");
+      }
+
+      free(g_pInFile);
+      return NULL;
+   }
+
+   if (pScan_type)
+      *pScan_type = image_info.m_scanType;
+
+   // In reduce mode output 1 pixel per 8x8 block.
+   decoded_width = reduce ? (image_info.m_MCUSPerRow * image_info.m_MCUWidth) / 8 : image_info.m_width;
+   decoded_height = reduce ? (image_info.m_MCUSPerCol * image_info.m_MCUHeight) / 8 : image_info.m_height;
+
+   // row_pitch = width byte size
+   row_pitch = decoded_width * image_info.m_comps;
+   pImage = (uint8 *)malloc(row_pitch * decoded_height);
+   if (!pImage)
+   {
+      free(g_pInFile);
+      return NULL;
+   }
+
+   row_blocks_per_mcu = image_info.m_MCUWidth >> 3;
+   col_blocks_per_mcu = image_info.m_MCUHeight >> 3;
+
+   for ( ; ; )
+   {
+      int y, x;
+      uint8 *pDst_row;
+
+      status = pjpeg_decode_mcu();
+
+      if (status)
+      {
+         if (status != PJPG_NO_MORE_BLOCKS)
+         {
+            Print(L"pjpeg_decode_mcu() failed with status %u\n", status);
+
+            free(pImage);
+            free(g_pInFile);
+            return NULL;
+         }
+
+         break;
+      }
+
+      if (mcu_y >= image_info.m_MCUSPerCol)
+      {
+         free(pImage);
+         free(g_pInFile);
+         return NULL;
+      }
+
+      if (reduce)
+      {
+         // In reduce mode, only the first pixel of each 8x8 block is valid.
+         pDst_row = pImage + mcu_y * col_blocks_per_mcu * row_pitch + mcu_x * row_blocks_per_mcu * image_info.m_comps;
+         if (image_info.m_scanType == PJPG_GRAYSCALE)
+         {
+            *pDst_row = image_info.m_pMCUBufR[0];
+         }
+         else
+         {
+            // uint y, x;
+            for (y = 0; y < col_blocks_per_mcu; y++)
+            {
+               uint src_ofs = (y * 128U);
+               for (x = 0; x < row_blocks_per_mcu; x++)
+               {
+                  pDst_row[0] = image_info.m_pMCUBufR[src_ofs];
+                  pDst_row[1] = image_info.m_pMCUBufG[src_ofs];
+                  pDst_row[2] = image_info.m_pMCUBufB[src_ofs];
+                  pDst_row += 3;
+                  src_ofs += 64;
+               }
+
+               pDst_row += row_pitch - 3 * row_blocks_per_mcu;
+            }
+         }
+      }
+      else
+      {
+         // Copy MCU's pixel blocks into the destination bitmap.
+         pDst_row = pImage + (mcu_y * image_info.m_MCUHeight) * row_pitch + (mcu_x * image_info.m_MCUWidth * image_info.m_comps);
+
+         for (y = 0; y < image_info.m_MCUHeight; y += 8)
+         {
+            const int by_limit = min(8, image_info.m_height - (mcu_y * image_info.m_MCUHeight + y));
+
+            for (x = 0; x < image_info.m_MCUWidth; x += 8)
+            {
+               uint8 *pDst_block = pDst_row + x * image_info.m_comps;
+
+               // Compute source byte offset of the block in the decoder's MCU buffer.
+               uint src_ofs = (x * 8U) + (y * 16U);
+               const uint8 *pSrcR = image_info.m_pMCUBufR + src_ofs;
+               const uint8 *pSrcG = image_info.m_pMCUBufG + src_ofs;
+               const uint8 *pSrcB = image_info.m_pMCUBufB + src_ofs;
+
+               const int bx_limit = min(8, image_info.m_width - (mcu_x * image_info.m_MCUWidth + x));
+
+               if (image_info.m_scanType == PJPG_GRAYSCALE)
+               {
+                  int bx, by;
+                  for (by = 0; by < by_limit; by++)
+                  {
+                     uint8 *pDst = pDst_block;
+
+                     for (bx = 0; bx < bx_limit; bx++)
+                        *pDst++ = *pSrcR++;
+
+                     pSrcR += (8 - bx_limit);
+
+                     pDst_block += row_pitch;
+                  }
+               }
+               else
+               {
+                  int bx, by;
+                  for (by = 0; by < by_limit; by++)
+                  {
+                     uint8 *pDst = pDst_block;
+
+                     for (bx = 0; bx < bx_limit; bx++)
+                     {
+                        pDst[0] = *pSrcR++;
+                        pDst[1] = *pSrcG++;
+                        pDst[2] = *pSrcB++;
+                        pDst += 3;
+                     }
+
+                     pSrcR += (8 - bx_limit);
+                     pSrcG += (8 - bx_limit);
+                     pSrcB += (8 - bx_limit);
+
+                     pDst_block += row_pitch;
+                  }
+               }
+            }
+
+            pDst_row += (row_pitch * 8);
+         }
+      }
+
+      mcu_x++;
+      if (mcu_x == image_info.m_MCUSPerRow)
+      {
+         mcu_x = 0;
+         mcu_y++;
+      }
+   }
+
+   free(g_pInFile);
+
+   *ix = decoded_width;
+   *iy = decoded_height;
+   *comps = image_info.m_comps;
+
+   return pImage;
+}
+//------------------------------------------------------------------------------
+static void get_pixel(int* pDst, const uint8 *pSrc, int luma_only, int num_comps)
+{
+   int r, g, b;
+   if (num_comps == 1)
+   {
+      r = g = b = pSrc[0];
+   }
+   else if (luma_only)
+   {
+      const int YR = 19595, YG = 38470, YB = 7471;
+      r = g = b = (pSrc[0] * YR + pSrc[1] * YG + pSrc[2] * YB + 32768) / 65536;
+   }
+   else
+   {
+      r = pSrc[0]; g = pSrc[1]; b = pSrc[2];
+   }
+   pDst[0] = r; pDst[1] = g; pDst[2] = b;
+}
+
+//------------------------------------------------------------------------------
+#define EXIT_FAILURE NULL
+
+static void* decode_jpeg(void* buffer, UINTN size)
+{
+   int width, height, comps;
+   pjpeg_scan_type_t scan_type;
+   uint8 *pImage;
+   int reduce = 0;
+   UINT16 *p = L"";
+
+   pImage = pjpeg_load_from_file(buffer, size, &width, &height, &comps, &scan_type, reduce);
+   if (!pImage)
+   {
+      Print(L"Failed loading source image!\n");
+      return EXIT_FAILURE;
+   }
+
+   Debug(L"Width: %d, Height: %d, Comps: %d\n", width, height, comps);
+
+   switch (scan_type)
+   {
+      case PJPG_GRAYSCALE: p = L"GRAYSCALE"; break;
+      case PJPG_YH1V1: p = L"H1V1"; break;
+      case PJPG_YH2V1: p = L"H2V1"; break;
+      case PJPG_YH1V2: p = L"H1V2"; break;
+      case PJPG_YH2V2: p = L"H2V2"; break;
+   }
+   Debug(L"Scan type: %s\n", p);
+
+	BMP* bmp = init_bmp(width, height);
+	if (!bmp) {
+		Print(L"HackBGRT: Failed to init_bmp\n");
+		free(pImage);
+		return 0;
+	}
+
+	int x, y, d;
+	int a[3];
+	UINT32 bmp_width = ((width * 3) + (width & 3));
+	for (y = 0; y != height; ++y) {
+        int pImagePos = (y * width) * comps;
+		for (x = 0; x != width; ++x) {
+			get_pixel(a, &pImage[pImagePos], (scan_type == PJPG_GRAYSCALE), comps);
+            pImagePos += comps;
+
+			// Debug(L"HackBGRT: bmp (%4d, %4d) #%04x.\n", x, y, a[0]);
+
+			UINT32 bmp_pos = bmp_width * (height - y - 1) + (x * 3) + 54;
+			for (d = 2; d >= 0; --d) {
+				// B,G,R
+				UINT8 c = (UINT8)(a[d] & 0xFF);
+				((UINT8*)bmp)[bmp_pos] = c;
+				++bmp_pos;
+			}
+
+			// Debug
+			if ((x % 32) || (y % 32) || (x > 256) || (y > 256))
+				continue;
+
+			// B,G,R
+			UINT8 r = ((UINT8*)bmp)[--bmp_pos];
+			UINT8 g = ((UINT8*)bmp)[--bmp_pos];
+			UINT8 b = ((UINT8*)bmp)[--bmp_pos];
+			Debug(L"HackBGRT: bmp (%4d, %4d) #%02x%02x%02x.\n", x, y, r, g, b);
+		}
+	}
+
+   free(pImage);
+
+   return bmp;
+}
+
+static BMP* LoadJPEG(EFI_FILE_HANDLE root_dir, const CHAR16* path) {
+    void* buffer = 0;
+    Debug(L"HackBGRT: Loading JPEG %s.\n", path);
+    UINTN size;
+    buffer = LoadFile(root_dir, path, &size);
+    if (!buffer) {
+        Print(L"HackBGRT: Failed to load JPEG (%s)!\n", path);
+        BS->Stall(1000000);
+        return 0;
+    }
+
+    BMP* bmp = decode_jpeg(buffer, size);
+    if (!bmp) {
+        FreePool(buffer);
+        Print(L"HackBGRT: Failed to decoce JPEG (%s)!\n", path);
+        BS->Stall(1000000);
+        return 0;
+    }
+
+    return bmp;
+}
+
 /**
  * Load a bitmap or generate a black one.
  *
@@ -572,18 +985,27 @@ static BMP* LoadBMP(EFI_FILE_HANDLE root_dir, const CHAR16* path) {
 	Debug(L"HackBGRT: Loading %s.\n", path);
 
 	UINTN len = StrLen(path);
-	CHAR16 last_char = path[len -1];
-	Debug(L"HackBGRT: Filename Len %d, Last Char %c.\n", (int)len, last_char);
-	if (last_char == 'g' || last_char == 'G') {
+	CHAR16 last_char_2 = path[len - 2];
+	Debug(L"HackBGRT: Filename Len %d, Last Char %c.\n", (int)len, last_char_2);
+	if (last_char_2 == 'm' || last_char_2 == 'M') {
+		// xxx.BMP
+		bmp = LoadFile(root_dir, path, 0);
+	} else if (last_char_2 == 'n' || last_char_2 == 'N') {
+		// xxx.PNG
 		bmp = LoadPNG(root_dir, path);
 	} else {
-		bmp = LoadFile(root_dir, path, 0);
+		// xxx.JPG
+		// xxx.JPEG
+		bmp = LoadJPEG(root_dir, path);
 	}
 	if (!bmp) {
 		Print(L"HackBGRT: Failed to load BMP (%s)!\n", path);
 		BS->Stall(1000000);
 		return 0;
 	}
+
+	Debug(L"HackBGRT: Load Success %s.\n", path);
+
 	return bmp;
 }
 
